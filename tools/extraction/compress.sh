0b1dfdfd9ef448494812a247d58913ea74d1271e
Pizza-Dox <diamond.nigel75@Gmail.com>
2015-04-10 00:50:21 +0530

    bugfixes

--- tools/extraction/compress.sh
+++ tools/extraction/compress.sh
@@ -1,4 +1,4 @@
 echo "recompressing data into .dat..."
 sleep 1
-./make_ext4fs -T 0 -S file_contexts -l 1073741824 -a system new_system.img kitchen/
+./make_ext4fs -T 0 -S file_contexts -l 1073741824 -a system new_system.img out/
 ./rimg2sdat new_system.img
--- tools/extraction/decompress.sh
+++ tools/extraction/decompress.sh
@@ -1,5 +1,4 @@
 echo "extracting data from .dat files..."
 sleep 1
-./sdat2img system.transfer.list system.new.dat system.img
-sudo mount -t ext4 -o loop system.img kitchen/
-echo "contents are in kitchen folder"
+./sdat2img.py system.transfer.list system.new.dat system.img
+sudo mount -t ext4 -o loop system.img out/
